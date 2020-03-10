26ce41bf45ca6abb3258ebbe567c212f5fb22882
Kanciano <krystianantkowiak001@gmail.com>
2020-03-10 20:22:12 +0100

    save and delete

--- app.js
+++ app.js
@@ -1,12 +1,74 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config();
+mongoose.Promise = global.Promise;
+
 const app = express();
+const mongoUri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_USERNAME}.documents.azure.com:${process.env.DB_PORT}/${process.env.DB_NAME}?ssl=true`;
 
-const port = process.env.PORT || 3000;
+mongoose.set('debug', true);
+mongoose.connect(mongoUri, { useUnifiedTopology: true, useNewUrlParser: true });
 
-app.use(express.static('public'));
+const Url = mongoose.model('Url', new mongoose.Schema(
+  {
+    name: String
+  },
+  {
+    collection: 'Urls',
+    versionKey: false
+  }
+));
 
-app.get('/', (req, res) => {
-  res.sendFile('public/index.html', { root: './' });
+const Email = mongoose.model('Email', new mongoose.Schema(
+  {
+    name: String
+  },
+  {
+    collection: 'Emails',
+    versionKey: false
+  }
+));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'hbs')
+
+app.get('/', async (req, res) => {
+  const urls = await Url.find({}).exec();
+  const [email] = await Email.find({}).exec();
+  res.render('index', { urls, email });
 });
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.post('/save', async (req, res) => {
+  const email = req.body.email && req.body.email.trim();
+  if (email) {
+    const emails = await Email.find({}).exec();
+    if (emails.length === 0) {
+      const emailModel = new Email({ name: email });
+      await emailModel.save({ upsert: true });
+    }
+  }
+
+  const url = req.body.url && req.body.url.trim();
+  if (url) {
+    const urlModel = new Url({ name: url });
+    await urlModel.save({ upsert: true });
+  }
+  res.redirect('/');
+});
+
+app.post('/send', async (req, res) => {
+  res.redirect('/');
+})
+
+app.post('/delete', async (req, res) => {
+  await Url.findOneAndRemove({ _id: req.body.id });
+  res.redirect('/');
+})
+
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`));
